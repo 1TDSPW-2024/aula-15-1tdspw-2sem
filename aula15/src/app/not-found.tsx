fb7981030e3f4ee491aa96b63cfe7c67f537ca04
heitxrr <heitorrrocha04@outlok.com>
2024-10-03 19:32:29 -0300

    criando pagina de erro

---
+++ aula15/src/app/not-found.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function not-found() {
+  return (
+    <div>
+      <h1>404 - Not Found...</h1>
+      <p>O recurso que você estava tentando acessar, não está mais disponível...</p>
+    </div>
+  )
+}
--- aula15/src/app/page.tsx
+++ aula15/src/app/page.tsx
@@ -2,30 +2,7 @@ export default function Home() {
   return (
     <div className="prose lg:prose-xl">
       <h1>Página Principal</h1>
-      <div>
-        <form action="">
-        <select className="px-4 py-3 rounded-full">
-        <option> - </option>
-          <option>opção-1</option>
-          <option>opção-2</option>
-          <option>opção-3</option>
-        </select>
-        {/* Or change a checkbox color using text color utilities: */}
-        <input type="checkbox" className="rounded text-pink-500" />
-        <input type="email" className="form-input px-4 py-3 rounded-full" />
-
-        <select className="form-select px-4 py-3 rounded-full">
-          <option> - </option>
-          <option>opção-1</option>
-          <option>opção-2</option>
-          <option>opção-3</option>
-        </select>
-
-        <input
-          type="checkbox" className="form-checkbox rounded text-pink-500"
-        />
-        </form>
-      </div>
+      
     </div>
   );
 }
