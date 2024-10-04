dceab14e788af19d89912081b78afc39d52f3352
HeitorOrtega <rm557825@fiap.com.br>
2024-10-03 21:01:24 -0300

    Commitando

--- aula15/src/app/api/base-route/route.tsx
+++ aula15/src/app/api/base-route/route.tsx
@@ -1,9 +1,35 @@
-import { NextResponse } from "next/server";
+import { TipoLanche } from "@/types";
 import { promises as fs } from "fs";
+import { NextResponse } from "next/server";
 
 export async function GET() {
 
-    const file = await fs.readFile(process.cwd() + '/src/data/base.json','utf-8');
+    const file = await fs.readFile(process.cwd() + '/src/data/base.json', 'utf-8');
     const data = JSON.parse(file);
     return NextResponse.json(data);
+}
+
+export async function POST(request: Request) {
+
+    const file = await fs.readFile(process.cwd() + '/src/data/base.json', 'utf-8');
+    const produtos: TipoLanche[] = JSON.parse(file);
+
+
+    const produto: TipoLanche = await request.json();
+    console.log("PRODUTO: ", produto);
+
+    const novoId = produtos[produtos.length - 1].id + 1
+
+    produto.id = novoId;
+
+    produtos.push(produto);
+
+
+    const fileUpdate = JSON.stringify(produtos);
+
+    await fs.writeFile(process.cwd() + '/src/data/base.json', fileUpdate);
+
+
+    return (NextResponse.json(produto, { status: 201 }));
+
 }
