d2c15e0dab311aeb5fc434c369986717cf378f12
heitxrr <heitorrrocha04@outlok.com>
2024-10-03 21:03:58 -0300

    Apagando node modules e terminando a routetsx

--- aula15/src/app/api/base-route/route.tsx
+++ aula15/src/app/api/base-route/route.tsx
@@ -1,9 +1,38 @@
 import { NextResponse } from "next/server";
 import { promises as fs } from "fs";
+import { TipoLanche } from "@/types";
+import { console } from "inspector";
 
 export async function GET() {
 
     const file = await fs.readFile(process.cwd() + '/src/data/base.json','utf-8');
     const data = JSON.parse(file);
     return NextResponse.json(data);
+}
+
+export async function POST(request:Request) {
+
+    const file = await fs.readFile(process.cwd() + '/src/data/base.json','utf-8');
+    const produtos:TipoLanche[] = JSON.parse(file);
+
+    //Realizar o destructuring do objeto
+    //const{id, nome } = await request.json();
+    //console.log("ID: ", id );
+    //console.log("Nome: ", nome );
+
+    const produto:TipoLanche = await request.json();
+    console.log("Produto: ", produto);
+
+    const novoId = produtos[produtos.length + 1].id + 1;
+
+    produto.id = novoId;
+
+    produtos.push(produto);
+
+    const fileupdate = JSON.stringify(produtos);
+
+    await fs.writeFile(process.cwd() + '/src/data/base.json', fileupdate);
+
+
+    return NextResponse.json(produto, { status: 201 });
 }
