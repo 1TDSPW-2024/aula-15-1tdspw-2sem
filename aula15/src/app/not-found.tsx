09209be9a8617ae37ebc21ecb35c967a87f500ce
CauaMachad0 <caua.marcelo.machado@outlook.com>
2024-10-03 21:23:21 -0300

    POST

---
+++ aula15/src/app/not-found.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function NotFound() {
+  return (
+    <div>
+
+        <h1>404 - Not Found...</h1>
+        <p>O recurso que você estava tentando acessar, não está mais disponível.</p>
+
+    </div>
+  )
+}
