fa4a09b01d8419454d299df770707bbd30f7799b
Danie-Anx <danielcoimbra.anx@gmail.com>
2024-10-03 21:03:39 -0300

    Fim da aula - 03-10.

--- aula15/src/app/api/base-route/route.tsx
+++ aula15/src/app/api/base-route/route.tsx
@@ -1,9 +1,35 @@
 import { NextResponse } from "next/server";
 import { promises as fs } from "fs";
+import { json } from "stream/consumers";
+import Produto from "@/app/produtos/produto/[id]/page";
+import { TipoLanche } from "@/types";
 
 export async function GET() {
 
     const file = await fs.readFile(process.cwd() + '/src/data/base.json','utf-8');
     const data = JSON.parse(file);
     return NextResponse.json(data);
+}
+
+export async function POST(request : Request) {
+
+    const file = await fs.readFile(process.cwd() + '/src/data/base.json','utf-8');
+    const produtos: TipoLanche[] = JSON.parse(file);
+
+    // Realizar o destructuring no objeto request
+    // const{id, nome} = await request.json();
+    const produto: TipoLanche = await request.json();
+    console.log("PRODUTO : " , produto);
+
+    const novoId = produtos[produtos.length -1].id + 1
+
+    produto.id = novoId;
+
+    produtos.push(produto);
+
+    const fileUpdate = JSON.stringify(produtos);
+
+    await fs.writeFile(process.cwd() + '/src/data/base.json','utf-8');
+
+    return NextResponse.json(produto, {"status": 201});
 }
---
+++ aula15/src/app/not-found.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function NotFound() {
+  return (
+    <div>
+        <h1>404 - Not Found...</h1>
+        <p> o recurso que você estava tentando acessar, não está mais disponível. </p>
+    </div>
+  )
+}
