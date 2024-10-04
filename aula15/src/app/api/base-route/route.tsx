2b191cdd323f0c3752d2492ef26675d561fb5d73
IHateMyTiming <Leo.kawachi@gmail.com>
2024-10-03 21:37:13 -0300

    ds

--- aula15/src/app/api/base-route/route.tsx
+++ aula15/src/app/api/base-route/route.tsx
@@ -1,9 +1,27 @@
-import { NextResponse } from "next/server";
+import { TipoLanche } from "@/types";
 import { promises as fs } from "fs";
+import { NextResponse } from "next/server";
 
 export async function GET() {
 
-    const file = await fs.readFile(process.cwd() + '/src/data/base.json','utf-8');
+    const file = await fs.readFile(process.cwd() + '/src/data/base.json', 'utf-8');
     const data = JSON.parse(file);
     return NextResponse.json(data);
+}
+
+export async function POST(request: Request) {
+    const file = await fs.readFile(process.cwd() + '/src/data/base.json', 'utf-8');
+    const produtos: TipoLanche[] = JSON.parse(file);
+
+    const produto: TipoLanche = await request.json();
+
+    console.log("Produto: ", produto);
+
+    const novoId = produtos[produtos.length - 1].id + 1
+    produto.id + novoId;
+
+    produtos.push(produto);
+
+
+    return NextResponse.json({ msg: "Hello" }, { "status": 201 });
 }
---
+++ aula15/src/app/not-found.tsx
@@ -0,0 +1,9 @@
+export default function () {
+    return (
+
+        <div>
+            <h1>404 - Not found...</h1>
+            <p>O recurso que voce esta tentando acessar, nao esta mais disponivel</p>
+        </div>
+    )
+}
